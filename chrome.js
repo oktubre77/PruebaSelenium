e093452cefa2b9c3ed37bfe503d11e3b56c381bd
rodrigocadiz47 <rodrigocadiz47@gmail.com>
2021-12-27 10:28:37 -0300

    length of sizes fixed

--- chrome.js
+++ chrome.js
@@ -13,19 +13,19 @@ async function dexter(){
                
        //await driver.get(process.env.URL_PROD);
        //await driver.get(process.env.URL_DEV_DEXTER);
-       await driver.get('http://dexter.com.ar');                
+       await driver.get('https://storefront:storefront@dev.dexter.com.ar/');                
 
         //To send a search query by passing the value in searchString.
-        await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
+       await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
  
-        //Verify the page title and print it
-        var title = await driver.getTitle();
-        console.log('Title is:',title);
+       //Verify the page title and print it
+       var title = await driver.getTitle();
+       console.log('Title is:',title);
         
        //Elegir Producto random de la grilla, calculando un index random
        console.log("Elige el producto en la grilla")
        var grilla = await driver.findElements(By.className("image-container"));
-       const indexRandom = Math.round(Math.random()*grilla.length);
+       const indexRandom = Math.round(Math.random()*grilla.length-1);
        grilla[indexRandom].click();
 
         
@@ -39,15 +39,15 @@ async function dexter(){
               if(!f.includes('disabled'))enabledSizes.push(sizes[i])
        }
 
-       const indexSizeRandom = Math.round(Math.random()*enabledSizes.length);
+       const indexSizeRandom = enabledSizes.length==1 ? 0 : Math.round(Math.random()*enabledSizes.length-1);
+       console.log(indexSizeRandom);
        enabledSizes[indexSizeRandom].click();
 
-        
        //Boton agregar al carrito
        await driver.sleep(3000);
        console.log("Boton agregar al carrito")
-       await driver.findElement(By.xpath("//*[@id='maincontent']/div/div[2]/div[3]/div[5]/div[8]/div/div/div/div/div[1]/button")).click();
-      
+       //await driver.findElement(By.xpath("//*[@id='maincontent']/div/div[2]/div[3]/div[5]/div[8]/div/div/div/div/div[1]/button")).click();
+       await driver.findElement(By.className("add-to-cart")).click();
        //Ir al carrito
        console.log("Ir al carrito")
        await driver.sleep(4000);
@@ -55,16 +55,15 @@ async function dexter(){
        //Boton comprar del carrito
        console.log("Boton comprar del carrito")
        await driver.findElement(By.xpath("//*[@id='maincontent']/div[3]/div[1]/div[2]/div[9]/div/div/a")).click();
-    
        //Ingresar Mail
         console.log("Ingresa mail")
        var login = process.env.LOGIN_DEV;
-       await driver.findElement(By.name("loginEmail")).sendKeys(login,Key.RETURN);
+       await driver.findElement(By.name("loginEmail")).sendKeys('rodrigocadiz47@gmail.com',Key.RETURN);
 
         //Ingresa PAssword 
-        console.log("Ingresa password");
+       console.log("Ingresa password");
        var passw = process.env.PASSWORD_DEV;
-       await driver.findElement(By.name("loginPassword")).sendKeys(passw,Key.RETURN);
+       await driver.findElement(By.name("loginPassword")).sendKeys('plataforma5',Key.RETURN);
        
        await driver.sleep(5000);
        await driver.findElement(By.xpath("//*[@id='checkout-main']/div[3]/div[1]/div[8]/div/div/button[1]")).click();
@@ -75,8 +74,8 @@ async function dexter(){
         console.log('Formulario de Envio:',title);
 
         console.log("Seleccionar Tipo de Envio");
-        await driver.findElement(By.xpath("//*[@id='null']/fieldset[4]/div[2]/div[3]/label")).click();
-
+        //await driver.findElement(By.xpath("//*[@id='null']/fieldset[4]/div[2]/div[3]/label")).click();
+        await driver.findElement(By.className("shipping-method-option")).click();
         await driver.sleep(3000);
         
         //Ingresa CVC Tarjeta 
